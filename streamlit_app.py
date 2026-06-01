64b5f7ce9dfa0f9c87f7d86f5a4cc6c61b44995b
monolife25-cmyk <monolife25@gmail.com>
2026-06-01 14:51:04 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,8 +63,8 @@ api_key = ""
 try:
     if hasattr(st, "secrets") and st.secrets is not None:
         if "GROQ_API_KEY" in st.secrets:
-            api_key = st.secrets["Gsk_BkWM6hjCwzJf4wC4RJnbWGdyb3FYv0P5ABjSruZ3qJ0uXsqNk3Vc]
-        elif "OPENROUTER_API_KEY" in st.secrets:
+api_key = st.secrets["GROQ_API_KEY"]
+"OPENROUTER_API_KEY" in st.secrets:
             api_key = st.secrets["OPENROUTER_API_KEY"]
 except Exception:
     pass
